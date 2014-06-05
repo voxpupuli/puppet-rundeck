ded6dab92430db5865c3aac3fe55b48c1be2a146
liamjbennett <lbennett@opentable.com>
2014-06-05 10:33:26 +0100

    fixing issue running tests of puppet 3.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
+require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
+
+$:.unshift File.join(File.dirname(__FILE__),  'fixtures', 'modules', 'stdlib', 'lib')
+$:.unshift File.join(File.dirname(__FILE__),  'fixtures', 'modules', 'inifile', 'lib')
 
 SUPPORTED_FAMILIES = ['Debian','RedHat']
