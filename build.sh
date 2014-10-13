f507f82293da7a6b2e02f246e2b545ec78d6b1a6
liamjbennett <lbennett@opentable.com>
2014-10-13 11:57:06 +0100

    updating metadata.json

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-bundle exec rake test
-
-for i in `ls -l spec/acceptance/nodesets/ | awk '{if (NR>1)print $9}' | grep -v default.yml | sed -e 's/\.yml//g'`
-do
-  BEAKER_set=$i BEAKER_debug=yes bundle exec rspec spec/acceptance
-done
