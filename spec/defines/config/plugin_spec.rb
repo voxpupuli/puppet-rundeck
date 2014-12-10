41c89c3d809b44830dbd85bc10d4a8e752e074e4
Smith, Trevor <TSmith3@Entertainment.com>
2014-12-10 17:01:01 -0500

    add test for plugin define with ensure set to absent

--- spec/defines/config/plugin_spec.rb
+++ spec/defines/config/plugin_spec.rb
@@ -29,6 +29,29 @@ describe 'rundeck::config::plugin', :type => :define do
           'group'  => 'rundeck'
         )}
       end
+
+      describe "rundeck::config::plugin definition with ensure set to absent on #{osfamily}" do
+        name = 'rundeck-hipchat-plugin-1.0.0.jar'
+        source = 'http://search.maven.org/remotecontent?filepath=com/hbakkum/rundeck/plugins/rundeck-hipchat-plugin/1.0.0/rundeck-hipchat-plugin-1.0.0.jar'
+        plugin_dir = '/var/lib/rundeck/libext'
+
+        let(:title) { name }
+        let(:params) {{
+          'source' => source,
+          'ensure' => 'absent'
+        }}
+
+        let(:facts) {{
+          :osfamily        => 'Debian',
+          :serialnumber    => 0,
+          :rundeck_version => ''
+        }}
+
+        it { should contain_file("#{plugin_dir}/#{name}").with(
+          'ensure' => 'absent'
+          )
+        }
+      end
     end
   end
 end
