eebeab082d3f204c53cea90b81a6e14c6148fb5c
Tim Meusel <tim@bastelfreak.de>
2016-06-13 11:15:48 +0200

    rubocop: fix Style/RegexpLiteral

--- spec/classes/config/global/file_keystore_spec.rb
+++ spec/classes/config/global/file_keystore_spec.rb
@@ -35,11 +35,11 @@ describe 'rundeck' do
     it { should contain_file('/var/lib/rundeck/var/storage') }
 
     # content and meta data for passwords
-    it { should contain_file('/var/lib/rundeck/var/storage/content/keys/foo/bar/password_key.password').with_content(/gobbledygook/) }
+    it { should contain_file('/var/lib/rundeck/var/storage/content/keys/foo/bar/password_key.password').with_content(%r{gobbledygook}) }
     it { should contain_file('/var/lib/rundeck/var/storage/meta/keys/foo/bar/password_key.password').with_content(%r{application/x-rundeck-data-password}) }
 
     # content and meta data for public keys
-    it { should contain_file('/var/lib/rundeck/var/storage/content/keys/foo/bar/public_key.public').with_content(/ssh-rsa AAAAB3rhwL1EoAIuI3hw9wZL146zjPZ6FIqgZKvO24fpZENYnNfmHn5AuOGBXYGTjeVPMzwV7o0mt3iRWk8J9Ujqvzp45IHfEAE7SO2frEIbfALdcwcNggSReQa0du4nd user@localhost/) }
+    it { should contain_file('/var/lib/rundeck/var/storage/content/keys/foo/bar/public_key.public').with_content(%r{ssh-rsa AAAAB3rhwL1EoAIuI3hw9wZL146zjPZ6FIqgZKvO24fpZENYnNfmHn5AuOGBXYGTjeVPMzwV7o0mt3iRWk8J9Ujqvzp45IHfEAE7SO2frEIbfALdcwcNggSReQa0du4nd user@localhost}) }
     it { should contain_file('/var/lib/rundeck/var/storage/meta/keys/foo/bar/public_key.public').with_content(%r{application/pgp-keys}) }
   end
 end
--- spec/functions/validate_rd_policy/signature_spec.rb
+++ spec/functions/validate_rd_policy/signature_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe 'validate_rd_policy' do
   describe 'signature validation' do
     it { is_expected.not_to eq(nil) }
-    it { is_expected.to run.with_params.and_raise_error(Puppet::ParseError, /wrong number of arguments/i) }
+    it { is_expected.to run.with_params.and_raise_error(Puppet::ParseError, %r{wrong number of arguments}i) }
 
     describe 'basic invalid inputs' do
-      it { is_expected.to run.with_params(1).and_raise_error(Puppet::ParseError, /is not a Hash or Array of hashes/) }
-      it { is_expected.to run.with_params(true).and_raise_error(Puppet::ParseError, /is not a Hash or Array of hashes/) }
-      it { is_expected.to run.with_params('one').and_raise_error(Puppet::ParseError, /is not a Hash or Array of hashes/) }
+      it { is_expected.to run.with_params(1).and_raise_error(Puppet::ParseError, %r{is not a Hash or Array of hashes}) }
+      it { is_expected.to run.with_params(true).and_raise_error(Puppet::ParseError, %r{is not a Hash or Array of hashes}) }
+      it { is_expected.to run.with_params('one').and_raise_error(Puppet::ParseError, %r{is not a Hash or Array of hashes}) }
     end
   end
 end
