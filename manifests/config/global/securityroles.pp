a013e1dfe54b82de11bb78776be73c9cd3840487
Zoltan Lanyi <zoltan.lanyi@1und1.de>
2016-06-28 13:55:12 +0200

    fix: add space between array elements

--- manifests/config/global/securityroles.pp
+++ manifests/config/global/securityroles.pp
@@ -8,7 +8,7 @@ define rundeck::config::global::securityroles ($security_role = $title) {
     lens    => 'Xml.lns',
     incl    => $rundeck::params::web_xml,
     onlyif  => "match web-app/security-role/role-name[#text = '${security_role}'] size == 0",
-    changes => [ "set web-app/security-role/#text[last()] '\t\t'","set web-app/security-role/role-name[last()+1]/#text '${security_role}'","set web-app/security-role/#text[last()+1] '\t'" ]
+    changes => [ "set web-app/security-role/#text[last()] '\t\t'", "set web-app/security-role/role-name[last()+1]/#text '${security_role}'", "set web-app/security-role/#text[last()+1] '\t'" ]
   }
 }
 
