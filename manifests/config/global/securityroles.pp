1b97c44e5844332d5e5dd1e11aa1ee39123b7723
Zoltan Lanyi <zoltan.lanyi@1und1.de>
2016-06-28 08:32:33 +0200

    merge: from test branch

--- manifests/config/global/securityroles.pp
+++ manifests/config/global/securityroles.pp
@@ -1,10 +1,10 @@
 #
 # Author: Zoltan Lanyi <zoltan.lanyi@gmail.com>
-# Date  : 06.06.2016
+# Date  : 03.06.2016
 # 
 
 define rundeck::config::global::securityroles ($security_role = $title) {
-  augeas { "rundeck/web.xml/security-role/role-name/$security_role":
+  augeas { "rundeck/web.xml/security-role/role-name/${security_role}":
     lens    => 'Xml.lns',
     incl    => $rundeck::params::web_xml,
     onlyif  => "match web-app/security-role/role-name[#text = '${security_role}'] size == 0",
