7053f5caa8034958ae93ab412a6f82e9f665336f
Zoltan Lanyi <zoltan.lanyi@1und1.de>
2016-06-29 09:33:50 +0200

    rename name, remove context

--- spec/defines/config/securityroles_spec.rb
+++ spec/defines/config/securityroles_spec.rb
@@ -6,14 +6,12 @@ describe 'rundeck::config::global::securityroles', type: :define do
       describe "rundeck::config::global::securityroles definition with array on #{osfamily}" do
         let(:title) { 'test' }
         let(:params) do
-          { rundeck_config_global_web_sec_roles: %w(DevOps roots) }
+          { name: %w(DevOps roots) }
         end
 
-        context 'test security roles with define' do
-          it 'generates augeas resource with specified security_roles' do
-            should contain_augeas('rundeck/web.xml/security-role/role-name') .with_changes(["set web-app/security-role/role-name/#text 'DevOps'"])
-            should contain_augeas('rundeck/web.xml/security-role/role-name') .with_changes(["set web-app/security-role/role-name/#text 'roots'"])
-          end
+	it 'generates augeas resource with specified security_roles' do
+          should contain_augeas('rundeck/web.xml/security-role/role-name') .with_changes(["set web-app/security-role/role-name/#text 'DevOps'"])
+          should contain_augeas('rundeck/web.xml/security-role/role-name') .with_changes(["set web-app/security-role/role-name/#text 'roots'"])
         end
       end
     end
