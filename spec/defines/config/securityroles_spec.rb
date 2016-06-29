4f09c2ffce319a8b6e39db2fd5b7d71a0598d898
Zoltan Lanyi <zoltan.lanyi@1und1.de>
2016-06-29 08:53:02 +0200

    rewrite

--- spec/defines/config/securityroles_spec.rb
+++ spec/defines/config/securityroles_spec.rb
@@ -1,22 +1,21 @@
 require 'spec_helper'
 
 describe 'rundeck::config::global::securityroles', type: :define do
-test_roles = %w(DevOps roots)
-let(:title) { name }
+  context 'supported operating systems' do
+    %w(Debian RedHat).each do |osfamily|
+      describe "rundeck::config::global::securityroles definition with array on #{osfamily}" do
+        let(:title) { name }
+        let(:params) do
+          { rundeck_config_global_web_sec_roles: %w(DevOps roots) }
+        end
 
-  context 'test security roles with define' do
-    let(:params) do
-      {
-        rundeck_config_global_web_sec_roles: test_roles
-      }
-    end
-
-    it 'generates augeas resource with specified security_roles' do
-      should contain_augeas('rundeck/web.xml/security-role/role-name') .with_changes(["set web-app/security-role/role-name/#text 'DevOps'"])
-      should contain_augeas('rundeck/web.xml/security-role/role-name') .with_changes(["set web-app/security-role/role-name/#text 'roots'"])
+	context 'test security roles with define' do
+          it 'generates augeas resource with specified security_roles' do
+            should contain_augeas('rundeck/web.xml/security-role/role-name') .with_changes(["set web-app/security-role/role-name/#text 'DevOps'"])
+            should contain_augeas('rundeck/web.xml/security-role/role-name') .with_changes(["set web-app/security-role/role-name/#text 'roots'"])
+          end
+        end
+      end
     end
   end
 end
-
-
-
