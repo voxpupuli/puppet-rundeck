468e2c0c069de573b41d8396ff3feaa89019bfdb
Zoltan Lanyi <zoltan.lanyi@1und1.de>
2016-06-29 09:09:44 +0200

    title is a string

--- spec/defines/config/securityroles_spec.rb
+++ spec/defines/config/securityroles_spec.rb
@@ -4,12 +4,12 @@ describe 'rundeck::config::global::securityroles', type: :define do
   context 'supported operating systems' do
     %w(Debian RedHat).each do |osfamily|
       describe "rundeck::config::global::securityroles definition with array on #{osfamily}" do
-        let(:title) { name }
+        let(:title) { 'test' }
         let(:params) do
           { rundeck_config_global_web_sec_roles: %w(DevOps roots) }
         end
 
-	context 'test security roles with define' do
+        context 'test security roles with define' do
           it 'generates augeas resource with specified security_roles' do
             should contain_augeas('rundeck/web.xml/security-role/role-name') .with_changes(["set web-app/security-role/role-name/#text 'DevOps'"])
             should contain_augeas('rundeck/web.xml/security-role/role-name') .with_changes(["set web-app/security-role/role-name/#text 'roots'"])
