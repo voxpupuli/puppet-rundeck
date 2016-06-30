b404835762518ca8c3503360b26b2a057aea17a8
Zoltan Lanyi <zoltan.lanyi@1und1.de>
2016-06-30 14:57:43 +0200

    remove: space and new empty line

--- spec/defines/config/securityroles_spec.rb
+++ spec/defines/config/securityroles_spec.rb
@@ -18,12 +18,10 @@ describe 'rundeck::config::global::securityroles', type: :define do
             puppetversion: Puppet.version
           }
         end
-
-
         let(:title) { 'test' }
         it 'generates augeas resource with specified security_roles' do
-           should contain_augeas('rundeck/web.xml/security-role/role-name') .with_changes(["set web-app/security-role/role-name/#text 'DevOps'"])
-           should contain_augeas('rundeck/web.xml/security-role/role-name') .with_changes(["set web-app/security-role/role-name/#text 'roots'"])
+          should contain_augeas('rundeck/web.xml/security-role/role-name') .with_changes(["set web-app/security-role/role-name/#text 'DevOps'"])
+          should contain_augeas('rundeck/web.xml/security-role/role-name') .with_changes(["set web-app/security-role/role-name/#text 'roots'"])
         end
       end
     end
