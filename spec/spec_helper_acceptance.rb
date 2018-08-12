9b80e00251c4d587f089107de3a6c7b0c5acc915
Tim Meusel <tim@bastelfreak.de>
2018-08-12 19:56:29 +0200

    allow puppetlabs/apt 5.x

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -11,7 +11,7 @@ install_module_dependencies
 
 # Install additional modules for soft deps
 install_module_from_forge('puppetlabs-java', '>= 2.1.0 < 3.0.0')
-install_module_from_forge('puppetlabs-apt', '>= 4.1.0 < 5.0.0')
+install_module_from_forge('puppetlabs-apt', '>= 4.1.0 < 6.0.0')
 
 RSpec.configure do |c|
   c.formatter = :documentation
