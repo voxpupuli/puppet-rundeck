9eac6118776d686db0f6f4a3f6b3c7ee7b0bdf98
Jeff Braucher <jeff@braucher.net>
2020-12-23 11:57:03 -0700

    undo diff

--- manifests/install.pp
+++ manifests/install.pp
@@ -68,7 +68,7 @@ class rundeck::install {
         }
       }
 
-      ensure_packages(['rundeck'], { 'ensure' => $package_ensure, notify => Class['rundeck::service'] } )
+      ensure_packages(['rundeck'], { 'ensure' => $package_ensure, notify => Class['rundeck::service'] })
       ensure_packages(['rundeck-cli'], { 'ensure' => $cli_package_ensure } )
     }
     'Debian': {
