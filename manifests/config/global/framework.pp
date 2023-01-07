9f5692088a32c466e2ccdf638011b47b286803b3
Stéphanie Jaumotte <stephanie.jaumotte@insee.fr>
2023-01-07 15:10:30 +0100

    Replace templates "key = value" erb by unique template epp

--- manifests/config/global/framework.pp
+++ manifests/config/global/framework.pp
@@ -39,7 +39,7 @@ class rundeck::config::global::framework {
 
   file { $properties_file:
     ensure  => file,
-    content => template('rundeck/framework.properties.erb'),
+    content => epp('rundeck/keyval.epp', { 'kvlist' => $framework_config }),
     owner   => $user,
     group   => $group,
     mode    => '0640',
