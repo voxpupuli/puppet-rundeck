565f3a06ff9ce8f7a56b354b0bdc1bb71cedd41c
Nicolás Osorio <rnicolas.yo@gmail.com>
2023-09-18 17:00:50 -0300

    fix test identation

--- spec/classes/config/global/rundeck_config_spec.rb
+++ spec/classes/config/global/rundeck_config_spec.rb
@@ -60,14 +60,14 @@ describe 'rundeck' do
       end
 
       describe "rundeck::config::global::rundeck_config class with sync ldap user parameter on #{os}" do
-      value = true
-      security_hash = {
-        'syncLdapUser' => value
-      }
-      let(:params) { { security_config: security_hash } }
+        value = true
+        security_hash = {
+          'syncLdapUser' => value
+        }
+        let(:params) { { security_config: security_hash } }
 
-      it { is_expected.to contain_file('/etc/rundeck/rundeck-config.groovy').with_content(%r{rundeck\.security\.syncLdapUser = #{value}}) }
-    end
+        it { is_expected.to contain_file('/etc/rundeck/rundeck-config.groovy').with_content(%r{rundeck\.security\.syncLdapUser = #{value}}) }
+      end
 
       describe "rundeck::config::global::rundeck_config class with no security parameters on #{os}" do
         bool_value = true
