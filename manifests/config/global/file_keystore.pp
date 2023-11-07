cc1cdbf2c7a9f0ddbb94d57b8e9fa0f4a9223fca
Joris VAN ACOLEYEN <joris.vanacoleyen@sfpd.fgov.be>
2023-11-07 15:01:22 +0100

    Update documentation for all config/global/ files

--- manifests/config/global/file_keystore.pp
+++ manifests/config/global/file_keystore.pp
@@ -1,42 +1,6 @@
-# == Class rundeck::config::global::file_keystore
+# @api private
 #
-# This private class is called from rundeck::config, used to manage the keys of
-# the Rundeck key storage facility if a file-based backend is used
-#
-# === Parameters
-#
-# [*keys*]
-#   A hash of key data values, with a minimum of the following properties:
-#  * *$value*: the actual value of the key, either the plaintext password string for passwords, or the encrypted public/priviate key
-#  * *$path*: a string representing the relative path of the key to the 'keys' directory
-#  * *$data_type*: the data type of the key, one of 'password', 'public-key' or 'private-key'
-#  * *$content_type*: MIME type of the content, either 'application/x-rundeck-data-password', 'application/pgp-keys' (for public keys)
-#
-# [*path*]
-#   The Rundeck OS user group
-#
-# [*user*]
-#   The Rundeck OS user
-#
-# Example:
-# ```
-# {
-#   $key1 => {
-#      $value        => 'secret',
-#      $path         => 'myproject/passwords',
-#      $data_type    => 'password',
-#      $content_type => 'application/x-rundeck-data-password', },
-#   $key2 => {
-#      $value        => 'ssh-rsa Th1sIsn0tRe@alLyAnRSAk3y',
-#      $path         => 'myproject/pubkeys',
-#      $data_type    => 'public',
-#      $content_type => 'application/pgp-keys', },
-# }
-# ```
-#
-#
-# [*file_keystorage_dir*]
-#    The default base directory for file-based key storage
+# @summary This private class is used to manage the keys of the Rundeck key storage facility if a file-based backend is used.
 #
 class rundeck::config::global::file_keystore {
   assert_private()
