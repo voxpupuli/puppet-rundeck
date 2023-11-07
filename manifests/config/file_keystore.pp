ecbf1646c44338e917f0dc59f3421fb1e7405364
Joris VAN ACOLEYEN <joris.vanacoleyen@sfpd.fgov.be>
2023-11-07 12:37:51 +0100

    Update documentation for config/file_keystore.pp

--- manifests/config/file_keystore.pp
+++ manifests/config/file_keystore.pp
@@ -1,62 +1,62 @@
-# == Define rundeck::config::file_keystore
+# @summary This define will create the 'content' and 'meta' components for the key to be stored.
 #
-# This type will create the 'content' and 'meta' components for the key to
-# be stored, and currently supports password-based public keys.  Private
-# keys are also supported, but not recommended to be privisioned via this mechanism
-# without the proper security policies for the private key data in place
+# Currently supports password-based public keys.
+# Private keys are also supported, but not recommended to be privisioned via this mechanism
+# without the proper security policies for the private key data in place.
 #
-# === Parameters
+# @example Basic usage.
+#   rundeck::config::file_keystore { 'mypassword':
+#     path         => 'myproject/mypassword',
+#     value        => 'secret',
+#     content_type => 'application/x-rundeck-data-password',
+#     data_type    => 'password',
+#   }
 #
-# [*auth_created_username*]
-#   User who created the key
-#
-# [*auth_modified_username*]
-#   User who last modified the key
-#
-# [*content_creation_time*]
-#   When the key was first created
-#
-# [*content_mask*]
-#   Content mask (default is 'content')
-#
-# [*content_size*]
-#   Size of the content string in bytes
-#
-# [*content_type*]
+# @param content_type
 #   MIME type of the content
-#
-# [*data_type*]
-#   Date type (password, public-key or private-key)
-#
-# [*file_keystorage_dir*]
+# @param data_type
+#   Data type (password, public-key or private-key)
+# @param path
+#   The path of the named key
+# @param value
+#   The actual value (password) of the named key
+# @param auth_created_username
+#   User who created the key
+# @param auth_modified_username
+#   User who last modified the key
+# @param content_creation_time
+#   When the key was first created
+# @param content_mask
+#   Content mask (default is 'content')
+# @param content_modify_time
+#   When the key was modified
+# @param content_size
+#   Size of the content string in bytes
+# @param file_keystorage_dir
 #   Base directory for file-based key storage (defaulted to /var/lib/rundeck/var/storage)
-#
-# [*group*]
-#   default system group for the Rundeck framework
-#
-# [*path*]
-#   The actual value (password) of the named key
-#
-# [*user*]
-#   default system user for the Rundeck framework
-#
-# [*value*]
-#   The actual value (password) of the named key
+# @param group
+#   Default system group for the Rundeck framework
+# @param user
+#   Default system user for the Rundeck framework
 #
 define rundeck::config::file_keystore (
-  Enum['application/x-rundeck-data-password', 'application/pgp-keys', 'application/octet-stream'] $content_type,
+  Enum[
+    'application/x-rundeck-data-password',
+    'application/pgp-keys',
+    'application/octet-stream'
+  ]                                     $content_type,
   Enum['password', 'public', 'private'] $data_type,
-  String $path,
-  String $value,
-  String $auth_created_username             = $rundeck::framework_config['framework.ssh.user'],
-  String $auth_modified_username            = $rundeck::framework_config['framework.ssh.user'],
-  String $content_creation_time             = chomp(generate('/bin/date', '+%Y-%m-%dT%H:%M:%SZ')),
-  String $content_mask                      = 'content',
-  String $content_modify_time               = chomp(generate('/bin/date', '+%Y-%m-%dT%H:%M:%SZ')),
-  Optional[Integer] $content_size           = undef,
-  Stdlib::Absolutepath $file_keystorage_dir = $rundeck::file_keystorage_dir,
-  String $group                             = $rundeck::config::group,
-  String $user                              = $rundeck::config::user,
+  String                                $path,
+  String                                $value,
+  String                                $auth_created_username  = $rundeck::framework_config['framework.ssh.user'],
+  String                                $auth_modified_username = $rundeck::framework_config['framework.ssh.user'],
+  String                                $content_creation_time  = chomp(generate('/bin/date', '+%Y-%m-%dT%H:%M:%SZ')),
+  String                                $content_mask           = 'content',
+  String                                $content_modify_time    = chomp(generate('/bin/date', '+%Y-%m-%dT%H:%M:%SZ')),
+  Optional[Integer]                     $content_size           = undef,
+  Stdlib::Absolutepath                  $file_keystorage_dir    = $rundeck::file_keystorage_dir,
+  String                                $group                  = $rundeck::config::group,
+  String                                $user                   = $rundeck::config::user,
 ) {
   ensure_resource('file', [$file_keystorage_dir], { 'ensure' => 'directory' })
 
