5710fbf03ca2d18c3ec31cb9019a768bf972fa39
Joris29 <joris.vanacoleyen@bryxx.eu>
2023-11-30 10:14:46 +0100

    Add sensitive for db

--- types/db_config.pp
+++ types/db_config.pp
@@ -3,7 +3,7 @@ type Rundeck::Db_config = Struct[{
     'url'                                  => String,
     Optional['driverClassName']            => String,
     Optional['username']                   => String,
-    Optional['password']                   => String,
+    Optional['password']                   => Variant[String[8], Sensitive[String[8]]],
     Optional['dialect']                    => String,
     Optional['properties.validationQuery'] => String,
 }]
