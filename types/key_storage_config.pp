1e52cf336ed526f6a28f1a48cf827819dea1c1c0
Joris29 <joris.vanacoleyen@bryxx.eu>
2023-12-05 12:52:11 +0100

    Fix key storage config removePathPrefix should be an optional parameter

--- types/key_storage_config.pp
+++ types/key_storage_config.pp
@@ -1,8 +1,9 @@
 # Rundeck key storage config type.
 type Rundeck::Key_storage_config = Array[
   Struct[{
-      'type'             => String,
-      'path'             => String,
-      Optional['config'] => Hash,
+      'type'                       => String,
+      'path'                       => String,
+      Optional['removePathPrefix'] => Boolean,
+      Optional['config']           => Hash,
   }]
 ]
