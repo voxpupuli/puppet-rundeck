147db65975defcc1cc34e370e2d767c7b64bcb0c
Joris29 <joris.vanacoleyen@bryxx.eu>
2024-01-23 08:17:29 +0100

    Test with latest 4.17

--- spec/acceptance/rundeck_spec.rb
+++ spec/acceptance/rundeck_spec.rb
@@ -35,7 +35,7 @@ describe 'rundeck class' do
     it 'applies successfully' do
       pp = <<-EOS
       class { 'rundeck':
-        package_ensure => 'latest'
+        package_ensure => '4.17.4.20231216-1'
       }
       EOS
 
